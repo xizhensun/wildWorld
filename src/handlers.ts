f49712ce1391892bb291cb0ab615e57102399904
xizhen sun <xizhensun@xizhens-MacBook-Pro.local>
2020-04-19 13:24:59 -0700

    google app engine pipeline

--- src/handlers.ts
+++ src/handlers.ts
@@ -9,7 +9,7 @@ type HelloBuilder = (name: string) => HelloResponse;
 const helloBuilder: HelloBuilder = name => ({ hello: name });
 
 export const rootHandler = (_req: Request, res: Response) => {
-  return res.send('API is working 🤓');
+  return res.send('API is not working anymore 🤓');
 };
 
 export const helloHandler = (req: Request, res: Response) => {
