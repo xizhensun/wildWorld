a523c3d50da310273ca12cf80ae11dfeb36750b1
xizhen sun <xizhensun@xizhens-MacBook-Pro.local>
2020-04-20 11:05:44 -0700

    add colyseus support

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,58 @@
+import path from 'path';
 import express from 'express';
-import { rootHandler, helloHandler } from './handlers';
+import serveIndex from 'serve-index';
+import cors from 'cors';
+import { createServer } from 'http';
+import { Server } from 'colyseus';
+import { monitor } from '@colyseus/monitor';
 
+// Import demo room handlers
+import { ChatRoom } from "./rooms/chat-room";
+import { StateHandlerRoom } from "./rooms/state-handler";
+import { AuthRoom } from "./rooms/auth-handler";
+import { ReconnectionRoom } from './rooms/reconnection';
+
+const port = Number(process.env.PORT || 2567) + Number(process.env.NODE_APP_INSTANCE || 0);
 const app = express();
-const port = process.env.PORT || '8000';
 
-app.get('/', rootHandler);
-app.get('/hello/:name', helloHandler);
+app.use(cors());
+app.use(express.json());
 
-app.listen(port, err => {
-  if (err) return console.error(err);
-  return console.log(`Server is listening on ${port}`);
+// Attach WebSocket Server on HTTP Server.
+const gameServer = new Server({
+  server: createServer(app),
+  express: app,
+  pingInterval: 0,
 });
+
+// Register ChatRoom as "chat"
+gameServer.define("chat", ChatRoom);
+
+// Register ChatRoom with initial options, as "chat_with_options"
+// onInit(options) will receive client join options + options registered here.
+gameServer.define("chat_with_options", ChatRoom, {
+    custom_options: "you can use me on Room#onCreate"
+});
+
+gameServer.define("state_handler", StateHandlerRoom);
+gameServer.define("auth", AuthRoom);
+gameServer.define("reconnection", ReconnectionRoom);
+
+app.use('/', express.static(path.join(__dirname, "static")));
+app.use('/', serveIndex(path.join(__dirname, "static"), {'icons': true}))
+
+// (optional) attach web monitoring panel
+app.use('/colyseus', monitor());
+
+gameServer.onShutdown(function(){
+  console.log(`game server is going down.`);
+});
+
+gameServer.listen(port);
+
+// process.on("uncaughtException", (e) => {
+//   console.log(e.stack);
+//   process.exit(1);
+// });
+
+console.log(`Listening on http://localhost:${ port }`);
